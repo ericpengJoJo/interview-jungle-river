c65bf3f4ee0cc20c36639c951283a1280a69332c
Jay Baugh <jay.baugh@rackspace.com>
2021-01-25 20:27:09 -0500

    Updated icons with color changing hover example

--- src/components/Icons/index.js
+++
@@ -1,2 +0,0 @@
-export * from 'grid-icon';
-export * from 'list-icon';
--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { ReactComponent as GridIcon} from './icon-grid.svg';
+import { ReactComponent as ListIcon} from './icon-list.svg';
 import "./ProductsPage.css";
 
 export const ProductsPage = () => {
   return (
-    <React.Fragment>
-      <div data-testid="product-page-heading" className="page-heading">Jungle River</div>
+    <div className="product-page">
+      <div data-testid="product-page-heading" className="page-heading">
+        Jungle River
+      </div>
       <div className="page-sub-heading">Products</div>
-    </React.Fragment>
+      <h4>Icons:</h4>
+      <div className="icons">
+        <ListIcon className="icon" title="list" />
+        <GridIcon className="icon" title="grid" />
+      </div>
+    </div>
   );
 };
