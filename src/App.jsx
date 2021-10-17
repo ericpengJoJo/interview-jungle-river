0ada4fba32051168d84506d502f0065d8e2cd3b8
CodeFuzzCC <60477360+CodeFuzzCC@users.noreply.github.com>
2021-10-17 13:54:06 -0400

    fetch api data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './scss/App.scss'
 import { ProductsPage } from './components/ProductsPage/ProductsPage';
+import { ProductContext } from './context/ProductContext'
 import "./App.css";
 
 export default function App() {
+  const [productList, setProductList] = useState([])
+  const [toggle, setToggle] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  const fetchData= async ()=>{
+    try{
+      const response = await fetch('https://fakestoreapi.com/products')
+      let apiData = await response.json()
+      setProductList(apiData)
+      setLoading(true)
+    }catch(err){
+      console.log("fetch fail!!")
+    }
+  }
+
+  useEffect(()=>{
+    fetchData()
+    console.log("fetch data =>", productList)
+  }, [toggle])
+
   return (
     <div className="App">
-      <ProductsPage />
+      <ProductContext.Provider value={{productList, toggle, setToggle, loading}}>
+        <ProductsPage />
+      </ProductContext.Provider>
     </div>
   );
 };
--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { ReactComponent as GridIcon} from './icon-grid.svg';
 import { ReactComponent as ListIcon} from './icon-list.svg';
-import "./ProductsPage.css";
+// import "./ProductsPage.css";
 
 export const ProductsPage = () => {
   return (
-    <div className="product-page">
+    <div className="container">
       <div data-testid="product-page-heading" className="page-heading">
         Jungle River
       </div>
-      <div className="page-sub-heading">Products</div>
-      <h4>Icons:</h4>
-      <div className="icons">
-        <ListIcon className="icon" title="list" />
-        <GridIcon className="icon" title="grid" />
+      <div className="nav">
+        <div className="page-sub-heading">Products</div>
+        <div className="icons">
+          <ListIcon className="icon" title="list" />
+          <GridIcon className="icon" title="grid" />
+        </div>
       </div>
+      
     </div>
   );
 };
