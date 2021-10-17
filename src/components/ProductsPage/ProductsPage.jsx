52c9274d84f4f094d52548e11912851206a64a87
CodeFuzzCC <60477360+CodeFuzzCC@users.noreply.github.com>
2021-10-17 14:13:31 -0400

    styling the toggle selector

--- src/components/ProductsPage/ProductsPage.jsx
+++ src/components/ProductsPage/ProductsPage.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useContext} from "react";
+import { ProductContext } from "../../context/ProductContext";
 import { ReactComponent as GridIcon} from './icon-grid.svg';
 import { ReactComponent as ListIcon} from './icon-list.svg';
 // import "./ProductsPage.css";
 
 export const ProductsPage = () => {
+
+  const {toggle, setToggle} = useContext(ProductContext)
+
   return (
     <div className="container">
       <div data-testid="product-page-heading" className="page-heading">
@@ -12,8 +16,16 @@ export const ProductsPage = () => {
       <div className="nav">
         <div className="page-sub-heading">Products</div>
         <div className="icons">
-          <ListIcon className="icon" title="list" />
-          <GridIcon className="icon" title="grid" />
+          <ListIcon 
+            className="icon" 
+            title="list"
+            onClick={e=> !toggle?setToggle(true):null}
+            />
+          <GridIcon 
+            className="icon" 
+            title="grid"
+            onClick={e=> toggle?setToggle(false):null}
+            />
         </div>
       </div>
       
